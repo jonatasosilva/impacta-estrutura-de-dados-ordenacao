52b275d4d4dbc3fa138608db7878a1dc602d995d
Jonatas Silva <54430451+jonatasosilva@users.noreply.github.com>
2022-05-09 02:52:39 -0300

    refactor: utiliza ArrayList no lugar da matriz nos vetores

--- algoritmosdeordenacao/Main.java
+++ algoritmosdeordenacao/Main.java
@@ -1,23 +1,25 @@
 package algoritmosdeordenacao;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         GeraNumerosAleatorios gna = new GeraNumerosAleatorios();
 
-        int[][] listas = new int[4][];
-        listas[0] = gna.geraNumerosAleatorios(100);
-        listas[1] = gna.geraNumerosAleatorios(1000);
-        listas[2] = gna.geraNumerosAleatorios(10000);
-        listas[3] = gna.geraNumerosAleatorios(100000);
-        // listas[4] = gna.geraNumerosAleatorios(1000000);
+        ArrayList<int[]> vetores = new ArrayList<>();
+        vetores.add(gna.geraNumerosAleatorios(100));
+        vetores.add(gna.geraNumerosAleatorios(1000));
+        vetores.add(gna.geraNumerosAleatorios(10000));
+        vetores.add(gna.geraNumerosAleatorios(100000));
+        // vetores.add(gna.geraNumerosAleatorios(1000000));
 
-        exibeCiclosPorAlgoritmoPorVetor("BubbleSort", listas);
-        exibeCiclosPorAlgoritmoPorVetor("InsertionSort", listas);
-        exibeCiclosPorAlgoritmoPorVetor("SelectionSort", listas);
+        exibeCiclosPorAlgoritmoPorVetor("BubbleSort", vetores);
+        exibeCiclosPorAlgoritmoPorVetor("InsertionSort", vetores);
+        exibeCiclosPorAlgoritmoPorVetor("SelectionSort", vetores);
     }
 
-    public static void exibeCiclosPorAlgoritmoPorVetor(String tipoAlgoritmo, int[][] vetores) {
+    public static void exibeCiclosPorAlgoritmoPorVetor(String tipoAlgoritmo, ArrayList<int[]> vetores) {
         AlgoritmoOrdenacaoFactory algoritmoFactory = new AlgoritmoOrdenacaoFactory();
         for (int[] vetor : vetores) {
             AlgoritmoOrdenacao algoritmo = algoritmoFactory.criaAlgoritmo(tipoAlgoritmo);
